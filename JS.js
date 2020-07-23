5e4c142a5fedb8939a8a22005d0e4a58ef397de0
SomNIA2 <leader0121@naver.com>
2020-07-23 16:46:55 +0900

    ~1.9

--- JS.js
+++ JS.js
@@ -1,3 +1,26 @@
-a = 21;
+/*const a = 21;
 b = a - 5;
-console.log(b);
+a = 4;
+console.log(a, b);
+*/
+/*
+String
+const what = "Isabell";
+
+console.log(what);
+*/
+/*
+Boolean;
+const i = true;
+*/
+/*
+Number
+const wat=666;
+ 
+ */
+/*
+//Float
+const wat = 55.5;
+console.log(wat);
+
+*/
