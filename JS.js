e3b958246a47b1c865f9ee71d0f307b6d7e08089
SomNIA2 <leader0121@naver.com>
2020-07-23 20:08:12 +0900

    ~1.10

--- JS.js
+++ JS.js
@@ -24,3 +24,30 @@ const wat = 55.5;
 console.log(wat);
 
 */
+const something = "Something";
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", something];
+console.log(daysOfWeek);
+
+const info = {
+  name: "SB",
+  age: 21,
+  gender: "male",
+  job: "Airman",
+  favMovies: ["Frozen2", "coco"],
+  favFood: [
+    {
+      name: "Kimchi",
+      fatty: false,
+    },
+    {
+      name: "Cheese burger",
+      fatty: false,
+    },
+  ],
+};
+console.log(info.gender);
+info.gender = "Female";
+
+console.log(info.gender);
+
+console.log(info);
