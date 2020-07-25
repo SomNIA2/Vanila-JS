52096ec25891e99182e0abc6cb2497d211815c5a
SomNIA2 <leader0121@naver.com>
2020-07-25 18:13:00 +0900

    ~3.1

--- JS.js
+++ JS.js
@@ -36,6 +36,8 @@ function handleClick() {
 }
 title.addEventListener("click", handleClick);
 */
+
+/* 2.5
 if (10 === 10 || 10 === 20) {
   console.log("hi");
 } else if ("10" === "10") {
@@ -53,4 +55,51 @@ if (age >= 18 && age <= 21) {
   console.log("go ahead");
 } else {
   console.log("u cant");
+}*/
+
+/* 2.6
+const title = document.querySelector("#title");
+
+const BASE_COLOR = " rgb(9, 14, 44)";
+const OTHER_COLOR = "#ee3123";
+
+function handleClick() {
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
 }
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handleClick);
+}
+init();
+function handleOffline() {
+  console.log("bye");
+}
+function handleOnline() {
+  console.log("hi! again");
+}
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
+*/
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+function handleClick() {
+  /* const hasClass = title.classList.contains(CLICKED_CLASS);
+  const currentClass = title.className;
+  if (hasClass) {
+    title.classList.remove(CLICKED_CLASS);
+  } else {
+    title.classList.add(CLICKED_CLASS);
+  }*/
+  title.classList.toggle(CLICKED_CLASS);
+  //클래스가 거기 있는지 체크해서 있으면 add 아니먄 remove를 해주는 것을 말한다.
+}
+function init() {
+  title.addEventListener("click", handleClick);
+}
+init();
