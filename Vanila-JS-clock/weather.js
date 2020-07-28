a20073eb0c7b80d84199044518edb19f4e936539
SomNIA2 <leader0121@naver.com>
2020-07-28 20:27:35 +0900

    cant

--- Vanila-JS-clock/weather.js
+++ Vanila-JS-clock/weather.js
@@ -1,4 +1,21 @@
+const weather = document.querySelector("js-selector");
+
+const API_KEY = "43612147ab058271d85475f71db404c2";
 const COORDS = "coords";
+
+function getweather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return console.log(response.json());
+    })
+    .then(function (json) {
+      const temperture = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperture}@${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +29,7 @@ function handleGeoSuccess(position) {
     //객체의 변수의 이름과 key의 이름이 같을 때는 latitude:latitude대신 latitude 한개만 써서 생략이 가능
   };
   saveCoords(coordsObj);
+  getweather(latitude, longitude);
 }
 function handleGeoError() {
   console.log("Cant access");
@@ -24,7 +42,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
-    //getweather
+    const parsedCoords = JSON.parse(loadedCoords);
+    getweather(parsedCoords.latitude, parsedCoords.longitude);
   }
 }
 
