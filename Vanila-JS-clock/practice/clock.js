8c11005310b5a28ef34082cdded65633dfa708da
SomNIA2 <leader0121@naver.com>
2020-07-31 21:02:43 +0900

    clock(am,pm must chg)

--- Vanila-JS-clock/practice/clock.js
+++ Vanila-JS-clock/practice/clock.js
@@ -5,9 +5,13 @@ function getTime() {
   const date = new Date();
   const min = date.getMinutes();
   const hours = date.getHours();
-  clockTitle.innerText = `${hours}:${min}`;
+  const sec = date.getSeconds();
+  clockTitle.innerText = `${hours >= 12 ? `PM ${hours}` : `AM ${hours}`}:${
+    min < 10 ? `0${min}` : min
+  }:${sec < 10 ? `0${sec}` : sec}`;
 }
 function init() {
+  setInterval(getTime, 1000);
   getTime();
 }
 init();
